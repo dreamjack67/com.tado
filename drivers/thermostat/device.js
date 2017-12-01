09ff909d9847f6aaba3b13ccc739331fcc09de62
Alex <openmind_nl@hotmail.com>
2017-12-01 17:24:17 +0100

    Add files via upload

--- drivers/thermostat/device.js
+++ drivers/thermostat/device.js
@@ -1,34 +1,226 @@
 'use strict';
 
 const TadoDevice = require('../../lib/TadoDevice');
+const Homey = require('homey');
 
 class TadoDeviceThermostat extends TadoDevice {
-	
+
+	triggerFlowHumidity( device, tokens ) {
+				this._flowTriggerHumidity
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowHeatingPower( device, tokens ) {
+				this._flowTriggerHeatingPower
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowOpenWindow( device, tokens ) {
+				this._flowTriggerOpenWindow
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowSmartHeating( device, tokens ) {
+				this._flowTriggerSmartHeating
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+
+	triggerFlowOutsideTemperature( device, tokens ) {
+				this._flowTriggerOutsideTemperature
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowSolarIntensity( device, tokens ) {
+				this._flowTriggerOutsideTemperature
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowWeather( device, tokens ) {
+				this._flowTriggerWeather
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+	triggerFlowPresence( device, tokens ) {
+				this._flowTriggerPresence
+						.trigger( device, tokens )
+								//.then( this.log )
+								.catch( this.error )
+	}
+
+
+
 	onInit() {
 		super.onInit();
-		
+//console.log(this)
+
 		this.registerCapabilityListener('target_temperature', this._onCapabilityTargetTemperature.bind(this))
 		this.registerCapabilityListener('tado_manual', this._onCapabilityTadoManual.bind(this))
-		
+
+		this._flowTriggerHumidity = new Homey.FlowCardTriggerDevice('humidity').register();
+		this._flowTriggerHeatingPower = new Homey.FlowCardTriggerDevice('heating_power').register();
+		this._flowTriggerSmartHeating = new Homey.FlowCardTriggerDevice('smart_heating').register();
+		this._flowTriggerOpenWindow = new Homey.FlowCardTriggerDevice('detect_open_window').register();
+		this._flowTriggerOutsideTemperature = new Homey.FlowCardTriggerDevice('outside_temperature').register();
+		this._flowTriggerSolarIntensity = new Homey.FlowCardTriggerDevice('solar_intensity').register();
+		this._flowTriggerWeather = new Homey.FlowCardTriggerDevice('weather').register();
+		this._flowTriggerPresence = new Homey.FlowCardTriggerDevice('presence_status').register();
+
 	}
-	
+
 	_onState( state ) {
-		
-		//console.log(state)
-		
-		if( this.hasCapability('measure_temperature') && state.sensorDataPoints.insideTemperature )
-			this.setCapabilityValue('measure_temperature', state.sensorDataPoints.insideTemperature.celsius ).catch( this.error );
-		
-		if( this.hasCapability('measure_humidity') && state.sensorDataPoints.humidity )
-			this.setCapabilityValue('measure_humidity', state.sensorDataPoints.humidity.percentage ).catch( this.error );
-		
-		if( this.hasCapability('target_temperature') && state.setting.temperature )
-			this.setCapabilityValue('target_temperature', state.setting.temperature.celsius ).catch( this.error );
-			
-		if( this.hasCapability('tado_manual') )
-			this.setCapabilityValue('tado_manual', state.overlayType === 'MANUAL' ).catch( this.error );
+		//console.log('zone state:'); console.log(state)
+		if( this.hasCapability('measure_temperature') && state.sensorDataPoints.insideTemperature ){
+			var value = Math.round( 10 * state.sensorDataPoints.insideTemperature.celsius )/10
+			if(this.getCapabilityValue('measure_temperature') !== value && value != undefined ){
+				//console.log('insideTemperature for ' + this.__name + ' changed to: ' + value)
+				this.setCapabilityValue('measure_temperature', value).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('measure_humidity') && state.sensorDataPoints.humidity ){
+			var value = Math.round( state.sensorDataPoints.humidity.percentage )
+			if(this.getCapabilityValue('measure_humidity') !== value && value != undefined ){
+				//console.log('Flow trigger for ' + this.__name + ': humidity changed to: ' + value)
+				this.triggerFlowHumidity( this, {'percentage': value } )
+				this.setCapabilityValue('measure_humidity', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('heating_power') && state.activityDataPoints.heatingPower ){
+			var value = Math.round( state.activityDataPoints.heatingPower.percentage )
+			if(this.getCapabilityValue('heating_power') !== value && value != undefined ){
+				//console.log('Flow trigger for ' + this.__name + ': heatingPower changed to: ' + value)
+				this.triggerFlowHeatingPower( this, {'percentage': value } )
+				this.setCapabilityValue('heating_power', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('detect_open_window') ){
+			var value = (state.openWindow !== null)
+			if(this.getCapabilityValue('detect_open_window') !== value && value != undefined ){
+				//console.log('Flow trigger for ' + this.__name + ': openWindow changed to: ' + value)
+				this.triggerFlowOpenWindow( this, {'detection': value } )
+				this.setCapabilityValue('detect_open_window', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('smart_heating') ){
+			var value = (state.overlayType !== 'MANUAL')
+			if(this.getCapabilityValue('smart_heating') !== value && value != undefined ){
+				//console.log('Flow trigger for ' + this.__name + ': SmartHeating (overlayType) changed to: ' + value)
+				this.triggerFlowSmartHeating( this, {'detection': value } )
+				this.setCapabilityValue('smart_heating', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('target_temperature') ){
+			if(state.setting.power == 'OFF' || !state.setting.temperature){
+				switch(state.setting.type){
+					case'HEATING':		var value = 5; break;
+					case'HOT_WATER':	var value = 30; break;
+				}
+			} else {
+				var value = Math.round( 10 * state.setting.temperature.celsius )/10;
+			}
+			if(this.getCapabilityValue('target_temperature') !== value ){
+				//console.log('target_temperature for ' + this.__name + ' changed to: ' + value);
+				this.setCapabilityValue('target_temperature', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('tado_manual') ){
+			var value = (state.overlayType === 'MANUAL')
+			if(this.getCapabilityValue('tado_manual') !== value ){
+				this.setCapabilityValue('tado_manual', value ).catch( this.error );
+			}
+		}
+
 	}
-	
+
+	_onWeather( state ) {
+		//console.log('home weather state:'); console.log(state);
+		if( this.hasCapability('measure_temperature.outside') && state.outsideTemperature ){
+			var value = Math.round( 10 * state.outsideTemperature.celsius )/10
+			if(this.getCapabilityValue('measure_temperature.outside') !== value ){
+				//console.log('Flow trigger for ' + this.__name + ': outsideTemperature changed to: ' + value)
+				this.triggerFlowOutsideTemperature( this, {'temperature': value } )
+				this.setCapabilityValue('measure_temperature.outside', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('solar_intensity') && state.solarIntensity ){
+			var value = Math.round( 10 * state.solarIntensity.percentage )/10
+			if(this.getCapabilityValue('solar_intensity') !== value ){
+				//console.log('Flow trigger for ' + this.__name + ': solarIntensity changed to: ' + value)
+				this.triggerFlowSolarIntensity( this, {'intensity': value } )
+				this.setCapabilityValue('solar_intensity', value ).catch( this.error );
+			}
+		}
+
+		if( this.hasCapability('weather_state') && state.weatherState ){
+			var value = (state.weatherState.value).toLowerCase()
+			if(this.getCapabilityValue('weather_state') != value ){
+				//console.log('Flow trigger for ' + this.__name + ': weatherState changed to: ' + value + ' (' + Homey.__( value ) + ')' )
+				this.triggerFlowWeather( this, {'condition': Homey.__( value ), 'state': value } )
+				this.setCapabilityValue('weather_state', value ).catch( this.error );
+			}
+		}
+
+	}
+
+	_onPresence( state ) {
+		//console.log('home presence state:'); console.log(state);
+		if( this.hasCapability('presence_status') ){
+			var value = false;
+			state.forEach(function(item, index){
+				//console.log(''); console.log(item);
+				if(item.settings.geoTrackingEnabled){
+					if(item.location != null){
+						value = (value || item.location.atHome)
+					}
+				}
+			});
+			if(this.getCapabilityValue('presence_status') != value ){
+				//console.log('Flow trigger for ' + this.__name + ': Presence changed to: ' + value )
+				this.triggerFlowPresence( this, {'presence': value } )
+				this.setCapabilityValue('presence_status', value ).catch( this.error );
+			}
+		}
+	}
+
+	onFlowActionSetSmart() {
+		this._api.unsetOverlay( this._homeId, this._zoneId);
+		return true;
+	}
+
+	onFlowActionSetOff() {
+		this._api.setOverlay( this._homeId, this._zoneId, {
+			"setting": {
+				"type": this._type,
+				"power": "OFF"
+			},
+			"termination": {
+				"type": "MANUAL"
+			}
+		});
+		return true;
+	}
+
 	async _onCapabilityTargetTemperature( value ) {
 		return this._api.setOverlay( this._homeId, this._zoneId, {
 			"setting": {
@@ -42,18 +234,17 @@ class TadoDeviceThermostat extends TadoDevice {
 				"type": "MANUAL"
 			}
 		}).then(() => {
-			return this.getState();
+			return Promise.resolve( this.getState() );
 		})
+		return true;
 	}
-	
+
 	async _onCapabilityTadoManual( value ) {
-		if( value ) {
-			return this._onCapabilityTargetTemperature( this.getCapabilityValue('target_temperature') );
-		} else {
-			return this._api.unsetOverlay( this._homeId, this._zoneId );
-		}
+			return this._api.unsetOverlay( this._homeId, this._zoneId ).then(() => {
+				return Promise.resolve( this.getState() );
+			});
 	}
-	
+
 }
 
 module.exports = TadoDeviceThermostat;
--- drivers/thermostat/driver.js
+++ drivers/thermostat/driver.js
@@ -1,32 +1,65 @@
 'use strict';
 
+const Homey = require('homey');
 const TadoDriver = require('../../lib/TadoDriver');
 
 const capabilitiesMap = {
-	'HEATING': [ 'tado_manual', 'measure_temperature', 'target_temperature', 'measure_humidity' ],
-	'HOT_WATER': [ 'tado_manual', 'target_temperature' ],
+	'HEATING': [
+		'tado_manual',
+		'target_temperature',
+		'measure_temperature',
+		'measure_humidity',
+		'heating_power',
+		'detect_open_window',
+		'smart_heating',
+		'measure_temperature.outside',
+		'solar_intensity',
+		'weather_state',
+		'presence_status',
+		'dummy_mobile'
+	],
+	'HOT_WATER': [
+		'tado_manual',
+		'target_temperature',
+		'smart_heating',
+		'measure_temperature.outside',
+		'solar_intensity',
+		'weather_state',
+		'presence_status',
+		'dummy_mobile'
+	],
 }
 const capabilitiesOptionsMap = {
 	'HEATING': {
-	    "target_temperature": {
+		"target_temperature": {
 		    "min": 5,
 		    "max": 25
-	    }		
+    },
+		"measure_temperature.outside": {
+			"title": {
+				"en": "Temperature outside",
+				"nl": "Temperatuur buiten"
+			}
+		}
 	},
 	'HOT_WATER': {
-	    "target_temperature": {
-		    "min": 30,
-		    "max": 65
-	    }
+    "target_temperature": {
+	    "min": 30,
+	    "max": 65
+    },
+		"measure_temperature.outside": {
+			"title": {
+				"en": "Temperature outside",
+				"nl": "Temperatuur buiten"
+			}
+		}
 	}
 }
+
 const mobileComponentsMap = {
 	'HEATING': [
 	    {
-		    "id": "icon"
-	    },
-	    {
-		    "id": "toggle",
+		    "id": "icon",
 		    "capabilities": [ "tado_manual" ],
 		    "options": {
 			    "showTitle": true
@@ -34,23 +67,109 @@ const mobileComponentsMap = {
 	    },
 	    {
 		    "id": "sensor",
-		    "capabilities": [ "measure_temperature", "measure_humidity" ]
+		    "capabilities": [
+					"measure_temperature",
+					"measure_humidity",
+					"heating_power",
+					"smart_heating",
+					"detect_open_window",
+					"presence_status",
+					"measure_temperature.outside",
+					"solar_intensity",
+					"weather_state",
+					"dummy_mobile"
+				],
+				"options": {
+					"showTitle": true,
+					"icons": {
+						"measure_temperature": "drivers/thermostat/assets/temp.svg",
+						"heating_power": "drivers/thermostat/assets/heating.svg",
+						"measure_humidity": "drivers/thermostat/assets/humidity.svg",
+						"detect_open_window": "drivers/thermostat/assets/open_window.svg",
+						"smart_heating": "drivers/thermostat/assets/smart_heating.svg",
+						"presence_status": "drivers/thermostat/assets/presence.svg",
+						"measure_temperature.outside": "drivers/thermostat/assets/temp_out.svg",
+						"solar_intensity": "drivers/thermostat/assets/sun.svg",
+						"weather_state": "drivers/thermostat/assets/weather.svg",
+						"dummy_mobile": "drivers/thermostat/assets/dummy.svg"
+					},
+					"detect_open_window": {
+						"noblink": false,
+						"invert": false,
+						"label": {
+								"true": { "en": "Heating paused", "nl": "Verwarming gepauzeerd" },
+								"false": { "en": "Not detected", "nl": "Niet gedetecteerd" }
+						}
+					},
+					"smart_heating": {
+						"noblink": false,
+						"invert": true,
+						"label": {
+								"true": { "en": "Active", "nl": "Actief" },
+								"false": { "en": "Not active", "nl": "Niet actief" }
+						}
+					},
+					"presence_status": {
+		        "noblink": false,
+		        "invert": true,
+						"label": {
+							"true": { "en": "Someone is at home", "nl": "Iemand is thuis" },
+							"false": { "en": "Everyone is out", "nl": "Iedereen is weg" }
+						}
+					}
+				}
 	    },
 	    {
 		    "id": "thermostat",
 		    "capabilities": [ "target_temperature" ]
 	    }
     ],
+
     'HOT_WATER': [
 	    {
-		    "id": "icon"
-	    },
-	    {
-		    "id": "toggle",
+		    "id": "icon",
 		    "capabilities": [ "tado_manual" ],
 		    "options": {
 			    "showTitle": true
 		    }
+	    },
+	    {
+		    "id": "sensor",
+		    "capabilities": [
+					"smart_heating",
+					"presence_status",
+					"measure_temperature.outside",
+					"solar_intensity",
+					"weather_state",
+					"dummy_mobile"
+				],
+				"options": {
+					"showTitle": true,
+					"icons": {
+						"smart_heating": "drivers/thermostat/assets/smart_heating.svg",
+						"measure_temperature.outside": "drivers/thermostat/assets/temp_out.svg",
+						"solar_intensity": "drivers/thermostat/assets/sun.svg",
+						"weather_state": "drivers/thermostat/assets/weather.svg",
+						"presence_status": "drivers/thermostat/assets/presence.svg",
+						"dummy_mobile": "drivers/thermostat/assets/dummy.svg"
+					},
+					"smart_heating": {
+						"noblink": false,
+						"invert": true,
+						"label": {
+								"true": { "en": "Active", "nl": "Actief" },
+								"false": { "en": "Not active", "nl": "Niet actief" }
+						}
+					},
+					"presence_status": {
+		        "noblink": false,
+		        "invert": true,
+						"label": {
+							"true": { "en": "Someone is at home", "nl": "Iemand is thuis" },
+							"false": { "en": "Everyone is out", "nl": "Iedereen is weg" }
+						}
+					}
+				}
 	    },
 	    {
 		    "id": "thermostat",
@@ -60,10 +179,39 @@ const mobileComponentsMap = {
 }
 
 class TadoDriverThermostat extends TadoDriver {
-	
+
+	onInit() {
+
+		new Homey.FlowCardCondition('weather_state')
+			.register()
+			.registerRunListener( (args, state) => { return args.current_state == args.device.getCapabilityValue('weather_state'); });
+
+		new Homey.FlowCardCondition('presence_status')
+			.register()
+			.registerRunListener( (args, state) => { return args.device.getCapabilityValue('presence_status'); });
+
+		new Homey.FlowCardCondition('smart_schedule')
+			.register()
+			.registerRunListener( (args, state) => { return args.device.getCapabilityValue('smart_heating'); });
+
+		new Homey.FlowCardCondition('open_window')
+			.register()
+			.registerRunListener( (args, state) => { return args.device.getCapabilityValue('detect_open_window'); });
+
+
+		new Homey.FlowCardAction('set_off')
+			.register()
+			.registerRunListener(args => args.device.onFlowActionSetOff());
+
+		new Homey.FlowCardAction('set_smart')
+			.register()
+			.registerRunListener(args => args.device.onFlowActionSetSmart());
+
+	}
+
 	async _onPairListDevices( result ) {
 		let devices = [];
-					
+
 		result.forEach( item => {
 			let home = item.home;
 			item.zones.forEach( zone => {
@@ -77,14 +225,13 @@ class TadoDriverThermostat extends TadoDriver {
 					device.capabilities = capabilitiesMap[ zone.type ];
 					device.capabilitiesOptions = capabilitiesOptionsMap[ zone.type ];
 					device.mobile = { components: mobileComponentsMap[ zone.type ] };
-								
+
 				devices.push(device);
-			})						
+			})
 		})
-		
 		return devices;
 	}
-	
+
 }
 
 module.exports = TadoDriverThermostat;
